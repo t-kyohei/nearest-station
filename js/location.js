d99a3bc67bffbf7c0013a788fd23b2083e49041b
unknown <li3151@snet.isid.co.jp>
2020-05-12 13:09:49 +0900

    最寄り駅アプリ

--- js/location.js
+++ js/location.js
@@ -4,7 +4,7 @@
   //let sensorg = new Gyroscope();
   //let geo = new GeolocationSensor({frequency: 30});
  
-  let sensor = new Sensor();
+  //let sensor = new Sensor();
   /*
   GeolocationSensor.read()
   .then(geo => console.log(`緯度: ${geo.latitude}, 経度: ${geo.longitude}`))
