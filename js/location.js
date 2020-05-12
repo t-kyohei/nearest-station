34743f183287aaa17b4956d80b7c6782c0626d02
unknown <li3151@snet.isid.co.jp>
2020-05-12 14:42:47 +0900

    最寄り駅アプリ

--- js/location.js
+++ js/location.js
@@ -48,7 +48,7 @@ navigator.permissions.query({ name: 'geolocation' }).then(result => {
     const geo = new GeolocationSensor({ frequency: 1 });
     geo.start();
 
-    geo.onreading = () => console.log(`緯度: ${geo.latitude}, 経度: ${geo.longitude}`);
+    geo.onreading = () => console.log(`緯度: ${geo.latitude}, 経度: ${geo.longitude},時刻: ${geo.timestamp}`);
 
     geo.onerror = event => console.error(event.error.name, event.error.message);
     //alert("テスト");
